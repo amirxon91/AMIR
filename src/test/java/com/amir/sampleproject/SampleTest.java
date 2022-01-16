180a8d180926e2b22413c7a83252ee3653e6db97
amirxon91 <amirxon91@gmail.com>
2022-01-16 12:46:34 -0500

    Add Sample Test

---
+++ src/test/java/com/amir/sampleproject/SampleTest.java
@@ -0,0 +1,14 @@
+package com.amir.sampleproject;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void paostJira() {
+		System.out.println("Sample Test");
+	}
+	@Test
+	public void deleteTwiter() {
+	System.out.println("Delete");	
+	}
+}
